2393e58d57c17b1bbc0ff1962bdaf2699e89571d
Gromtag <Gromtag@yandex.ru>
2023-09-09 00:20:38 +0000

    SAVE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,24 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://8ef8275fb1ec821396f00458c0e66189@o4505779615891456.ingest.sentry.io/4505779619430400",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+) 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['158.160.19.129', '127.0.0.1', 'localhost', 'gromtag.servebeer.com']
 
 # Application definition
 
@@ -103,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
